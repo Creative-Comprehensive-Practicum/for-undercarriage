014b262bb7afd54c2151d414c469931fc0ffc638
naga <toritonsuibu0527@gmail.com>
2023-02-02 16:46:07 +0900

    a

--- for-undercarriage.ino
+++ for-undercarriage.ino
@@ -1,3 +1,4 @@
+#define DEBUG
 #include <MsTimer2.h>
 
 template<typename first_t, typename second_t> 
@@ -14,6 +15,15 @@ public:
 
   first_t& first() {return first_element; }
   second_t& second() {return second_element; }
+};
+
+
+class output{
+public:
+  template <typename T>
+  output operator << (T out) const{
+    Serial.print(out);
+  }
 };
 
 namespace constants {
@@ -35,24 +45,22 @@ enum class PIN {
   analog5 = A5,
 };
 
-  constexpr uint8_t analog_thresholds[6] = {2, 2, 2, 2, 2, 2};
-  constexpr uint8_t weights[6] = {2, 1, 0, 0, -1, -2};
+  constexpr uint16_t analog_thresholds[4] = {3000, 10000, 8000, 10000};
+  constexpr uint8_t weights[4] = {2, 1, -1, -2};
 
-  constexpr uint8_t analog_pins[6] = {
+  constexpr uint8_t analog_pins[4] = {
     static_cast<uint8_t>(constants::PIN::analog0), 
     static_cast<uint8_t>(constants::PIN::analog1),
     static_cast<uint8_t>(constants::PIN::analog2),
     static_cast<uint8_t>(constants::PIN::analog3),
-    static_cast<uint8_t>(constants::PIN::analog4),
-    static_cast<uint8_t>(constants::PIN::analog5)
   };
 
 }
 
 namespace globals {
-  uint8_t loop_counter = 0;
+  uint16_t loop_counter = 0;
 
-  uint16_t analog_input[6] = {0, 0, 0, 0, 0, 0};
+  uint16_t analog_input[4] = {0, 0, 0, 0};
 
   pair<uint8_t, uint8_t> wheel_spd(200, 200);
 }
@@ -62,6 +70,10 @@ void timer_inturrepts100(void);
 
 
 void setup() {
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif //DEBUG
+
   pinMode(static_cast<int>(constants::PIN::LED1), OUTPUT);
   pinMode(static_cast<int>(constants::PIN::LED2), OUTPUT);
   pinMode(static_cast<int>(constants::PIN::LED3), OUTPUT);
@@ -71,7 +83,8 @@ void setup() {
   analogWrite(static_cast<int>(constants::PIN::PWM1), globals::wheel_spd.first());
   analogWrite(static_cast<int>(constants::PIN::PWM3), globals::wheel_spd.second());
 
-  MsTimer2::set(100, timer_inturrepts100);
+  MsTimer2::set(10, timer_inturrepts10);
+  MsTimer2::start();
 }
 
 void loop() {
@@ -79,26 +92,28 @@ void loop() {
 }
 
 void analog_reader() {
-  for(int i = 0; i < 6; i++){
+  for(int i = 0; i < 4; i++){
     globals::analog_input[i] += analogRead(constants::analog_pins[i]) << 8;
   }
 
   ++globals::loop_counter;
 }
 
-void timer_inturrepts100(void) {
-  uint8_t analog_integraled[6] = {0, 0, 0, 0, 0, 0};
-  bool analog_is_white[6];
-  
-  for(int i = 0; i < 6; i++){
-    analog_integraled[i] = (globals::analog_input[i] / globals::loop_counter) >> 8;
+void timer_inturrepts10(void) {
+#ifdef DEBUG
+  output() << globals::wheel_spd.first() << ", " << globals::wheel_spd.second() << '\n';
+#endif //DEBUG
 
-    analog_is_white[i] = (analog_integraled[i] > constants::analog_thresholds[i]) ? true : false;
+  uint8_t analog_integraled[4] = {0, 0, 0, 0};
+  uint8_t analog_is_white[4];
+  
+  for(int i = 0; i < 4; i++){
+    analog_is_white[i] = (globals::analog_input[i] > constants::analog_thresholds[i]) ? 1 : 0;
 
     globals::analog_input[i] = 0;
   }
 
-  for(int i = 0; i < 6; i++){
+  for(int i = 0; i < 4; i++){
     globals::wheel_spd.first() += constants::weights[i] * analog_is_white[i];
     globals::wheel_spd.second() += - constants::weights[i] * analog_is_white[i];
   }
@@ -107,5 +122,6 @@ void timer_inturrepts100(void) {
   analogWrite(static_cast<int>(constants::PIN::PWM3), globals::wheel_spd.second());
   
   globals::loop_counter = 0;
-  
+
+
 }
